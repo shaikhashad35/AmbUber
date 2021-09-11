cc2c8cae16da79f8f93826be92c78529097cf50c
Ashad <shaikhashad35@gmail.com>
2021-09-11 15:19:09 +0530

    First Commit

---
+++ src/main/java/com/example/AmbUber/AmbUberApplication.java
@@ -0,0 +1,13 @@
+package com.example.AmbUber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbUberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AmbUber/AmbUberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AmbUber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
